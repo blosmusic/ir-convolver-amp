5f61f3002ad027fcee565d61db8a8ed9be1ce8ae
BLOS <blosguitarist@gmail.com>
2023-05-12 02:02:32 +0100

    sliders attached

--- assets/controls.js
+++ assets/controls.js
@@ -1,38 +1,26 @@
 // Get the slider and knob elements
-const slider = document.querySelector(".amp-slider");
-const knob = document.querySelector(".amp-control");
-
-// Variables to track the slider state
-let isDragging = false;
+const sliderGroup = document.querySelector(".amp-slider");
+const knobs = document.querySelectorAll(".amp-control ");
 
 // Function to handle mouse or touch events
 function handleDrag(event) {
   event.preventDefault();
 
-  if (event.type === "mousedown" || event.type === "touchstart") {
-    isDragging = true;
-  } else if (event.type === "mouseup" || event.type === "touchend") {
-    isDragging = false;
-  }
+  // Get the parent slider ID
+  const sliderId = event.target.parentNode.id;
+  const sliderValue = event.target.value;
 
-  if (isDragging) {
-    // Calculate the angle based on the mouse or touch position
-    const sliderRect = slider.getBoundingClientRect();
-    const centerX = sliderRect.left + sliderRect.width / 2;
-    const centerY = sliderRect.top + sliderRect.height / 2;
-    const angle =
-      Math.atan2(event.clientY - centerY, event.clientX - centerX) *
-      (180 / Math.PI);
-
-    // Update the knob position
-    knob.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
-  }
+  // Rest of the code for handling slider interaction
+  // ...
+  console.log(sliderId + " is being dragged", sliderValue, "value");
 }
 
-// Add event listeners to handle dragging
-slider.addEventListener("mousedown", handleDrag);
-slider.addEventListener("mousemove", handleDrag);
-slider.addEventListener("mouseup", handleDrag);
-slider.addEventListener("touchstart", handleDrag);
-slider.addEventListener("touchmove", handleDrag);
-slider.addEventListener("touchend", handleDrag);
+// Add event listeners to handle dragging for each knob
+knobs.forEach((knob) => {
+  knob.addEventListener('mousedown', handleDrag);
+  knob.addEventListener('mousemove', handleDrag);
+  knob.addEventListener('mouseup', handleDrag);
+  knob.addEventListener('touchstart', handleDrag);
+  knob.addEventListener('touchmove', handleDrag);
+  knob.addEventListener('touchend', handleDrag);
+});
